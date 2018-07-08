71024e25560df118f564f8e6a49ec25cf7839d1e
ed@sharpertool.com <ed@sharpertool.com>
2018-07-07 19:27:12 -0600

    Disable pipeline. Fix debug toolbar

--- sharpertool/sharpertool/settings/common.py
+++ sharpertool/sharpertool/settings/common.py
@@ -62,7 +62,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'storages',
-    'pipeline',
+    #'pipeline',
     'raven.contrib.django.raven_compat',
 ]
 
