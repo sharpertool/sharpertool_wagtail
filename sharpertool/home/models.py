fe874182a8f606b4ecdb71f68fe063089fc514a5
ed@sharpertool.com <ed@sharpertool.com>
2018-07-08 13:51:20 -0600

    Add sharpertool home page model, update templates

--- sharpertool/home/models.py
+++ sharpertool/home/models.py
@@ -1,7 +1,33 @@
 from django.db import models
 
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    InlinePanel)
+from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
+from wagtail.core.models import Orderable
 
 
 class HomePage(Page):
-    pass
+    tagline = models.CharField(max_length=120, default='')
+
+    content_panels = [
+        FieldPanel('tagline'),
+        InlinePanel('highlights', label="Highlight Item")
+    ]
+
+
+class HomePageHighlight(Orderable):
+    page = ParentalKey(HomePage, on_delete=models.CASCADE, related_name='highlights')
+    name = models.CharField(max_length=30)
+    link = models.URLField()
+    text = RichTextField()
+
+    panels = [
+        FieldPanel('name'),
+        FieldPanel('link'),
+        FieldPanel('text', classname='full')
+    ]
+
+
