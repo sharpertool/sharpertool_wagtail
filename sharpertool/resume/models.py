3f95c6455c341529f3407b3b7b1a6a62f70bc743
ed@sharpertool.com <ed@sharpertool.com>
2018-07-08 22:27:02 -0600

    Updated education and project work areas.

--- sharpertool/resume/models.py
+++ sharpertool/resume/models.py
@@ -46,9 +46,93 @@ class ResumePage(Page):
             ImageChooserPanel('profile_image'),
         ], heading='Home Section', classname="collapsible collapsed"),
         MultiFieldPanel([
+            InlinePanel('project_item'),
+        ], heading='Project Work Items', classname='collapsible collapsed'),
+        MultiFieldPanel([
+            InlinePanel('education', heading='Educational Items'),
+            InlinePanel('skills', heading='Skills'),
+            InlinePanel('experience', heading='Experience Items'),
+        ], heading='Education and Experience', classname='collapsible collapsed'),
+        MultiFieldPanel([
             FieldPanel('contact_info'),
             FieldPanel('contact_email', heading="Enter value if different from main email"),
             FieldPanel('contact_longitude', heading='Longitude of Contact location'),
             FieldPanel('contact_latitude', heading='Latitude of Contact location'),
         ], heading='Contacts', classname="collapsible collapsed"),
+    ]
+
+    def get_context(self, request, *args, **kwargs):
+        context = super().get_context(request, *args, **kwargs)
+
+        return context
+
+
+class ResumeProjectItem(Orderable):
+
+    SECTION_CHOICES = (
+        ('webdesign', 'Web Design',),
+        ('engineering', 'Engineering',),
+        ('edatools', 'EDA Tools'),
+    )
+
+    page = ParentalKey(ResumePage, on_delete=models.CASCADE, related_name='project_item')
+    section = models.CharField(choices=SECTION_CHOICES, max_length=20, null=True, blank=True)
+    title = models.CharField(max_length=40)
+    subtext = models.CharField(max_length=80, null=True, blank=True)
+    thumb = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+    image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    panels = [
+        FieldPanel('section'),
+        FieldPanel('title'),
+        FieldPanel('subtext'),
+        ImageChooserPanel('image', heading='Large image for lightbox'),
+        ImageChooserPanel('thumb', heading='thumbnail image'),
+    ]
+
+
+class EducationItem(Orderable):
+    page = ParentalKey(ResumePage, on_delete=models.CASCADE, related_name='education')
+    title = models.CharField(max_length=120, null=True, blank=True)
+    start = models.CharField(max_length=20, null=True, blank=True)
+    end = models.CharField(max_length=20, null=True, blank=True)
+    text = RichTextField(null=True, blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('start', heading='Start month-year, or just year'),
+        FieldPanel('end', heading='End month-year, or just year'),
+        FieldPanel('text'),
+    ]
+
+
+class Skills(Orderable):
+    page = ParentalKey(ResumePage, on_delete=models.CASCADE, related_name='skills')
+    title = models.CharField(max_length=120, null=True, blank=True)
+    percent = models.CharField(max_length=5, null=True, blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('percent', heading='A percent value from 0-100, format: 56%'),
+    ]
+
+
+class Experience(Orderable):
+    page = ParentalKey(ResumePage, on_delete=models.CASCADE, related_name='experience')
+    title = models.CharField(max_length=120, null=True, blank=True)
+    start = models.CharField(max_length=20, null=True, blank=True)
+    end = models.CharField(max_length=20, null=True, blank=True)
+    text = RichTextField(null=True, blank=True)
+
+    panels = [
+        FieldPanel('title'),
+        FieldPanel('start', heading='Start month-year, or just year'),
+        FieldPanel('end', heading='End month-year, or just year'),
+        FieldPanel('text'),
     ]
