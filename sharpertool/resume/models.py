e34d75fd0b62b40d54ae8066f35b48e0aab2e64a
ed@sharpertool.com <ed@sharpertool.com>
2018-07-08 20:41:45 -0600

    Update contacts section

--- sharpertool/resume/models.py
+++ sharpertool/resume/models.py
@@ -3,15 +3,48 @@ from django.db import models
 from modelcluster.fields import ParentalKey
 from wagtail.admin.edit_handlers import (
     FieldPanel,
+    MultiFieldPanel,
     InlinePanel)
 from wagtail.core.fields import RichTextField
 from wagtail.core.models import Page
 from wagtail.core.models import Orderable
+from wagtail.images.edit_handlers import ImageChooserPanel
 
 
 class ResumePage(Page):
+    heading = RichTextField(default='')
+    intro = RichTextField(default='')
     name = models.CharField(max_length=128, default='')
+    dob = models.CharField(max_length=40, null=True, blank=True)
+    email = models.EmailField(default='')
+    address = models.CharField(max_length=50, default='')
+    website = models.URLField(null=True)
+    phone = models.CharField(max_length=20, default='')
+    profile_image = models.ForeignKey(
+        'wagtailimages.Image', null=True, blank=True,
+        on_delete=models.SET_NULL, related_name='+'
+    )
+
+    # Contact Information
+
+    contact_info = RichTextField(default='')
+    contact_email = models.EmailField(null=True, blank=True)
+
 
     content_panels = Page.content_panels + [
-        FieldPanel('name'),
+        MultiFieldPanel([
+            FieldPanel('heading'),
+            FieldPanel('intro'),
+            FieldPanel('name'),
+            FieldPanel('dob'),
+            FieldPanel('email'),
+            FieldPanel('address'),
+            FieldPanel('website'),
+            FieldPanel('phone'),
+            ImageChooserPanel('profile_image'),
+        ], heading='Home Section', classname="collapsible collapsed"),
+        MultiFieldPanel([
+            FieldPanel('contact_info'),
+            FieldPanel('contact_email', heading="Enter value if different from main email"),
+        ], heading='Contacts', classname="collapsible collapsed"),
     ]
--- sharpertool/static/js/resume_main.js
+++ sharpertool/static/js/resume_main.js
@@ -55,8 +55,8 @@ $(function () {
   "use strict";
 	var map = new GMaps({
 	el: "#map",
-	lat: 40.714353,
-	lng: -74.005973,
+	lat: 43.610591,
+	lng: -116.392550,
           zoom: 15, 
           zoomControl : true,
           zoomControlOpt: {
@@ -87,8 +87,8 @@ $(function () {
       map.setStyle("map_style");
 
       map.addMarker({
-        lat: 40.714353,
-        lng: -74.005973,
+        lat: 43.610591,
+        lng: -116.392550,
         icon: "/static/images/marker.png"
       });
 });
